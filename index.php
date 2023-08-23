b41a11ccec7b6f8c922602cae65fb50339ce9eea
jeanSaposhnikov <jean.saposhnikov@hotmail.fr>
2023-08-23 19:59:13 -0400

    array

--- index.php
+++ index.php
@@ -19,6 +19,16 @@
 
             Color : <input ng-model="color"  type="text">
             <input type="text" style="background-color:{{color}}" readonly value={{color}}>
+            <div>
+                <label>Quantity<input ng-model="amount" type="number" value="3"></label>
+                <label>Price<input ng-model="price" type="text" value="0"></label>
+                <p>Total : {{amount*price}}
+            </div>
+
+            <div ng-init="clients=['Robert','Celine','Azazel','Mick','Nicole']">
+                <p>{{clients}}</p>
+                <p>The person with the best name : {{clients[2]}}</p>
+            </div>
         </div>
     </body>
 </html>
