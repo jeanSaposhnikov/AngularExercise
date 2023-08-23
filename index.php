bc3b8fd1d68d880aeb695d218eece412afe1009f
jeanSaposhnikov <jean.saposhnikov@hotmail.fr>
2023-08-23 17:55:15 -0400

    Dynamic values in action

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
     <head>
-        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
     </head>
     <body>
-        <div ng-app="app">
+        <div ng-app="">
             <h1>app</h1>
             <p ng-model="model">Model</p>
+            <p ng-bind="model"></p>
+            <p>Expression outputted: {{expression}}</p>
+
+
+            <p>Name: <input type="text" ng-model="name"></p>
+            <p>{{name}}</p>
+
+            <p>Last Name: <input type="text" ng-model="last"></p>
+            <p ng-bind="last"></p>
+
+            Color : <input ng-model="color"  type="text">
+            <input type="text" style="background-color:{{color}}" readonly value={{color}}>
         </div>
     </body>
 </html>
