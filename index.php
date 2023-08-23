b3c73c349a1af9e1926cb3b82ab72deadcae6a41
jeanSaposhnikov <jean.saposhnikov@hotmail.fr>
2023-08-23 16:51:11 -0400

    Angular start

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+    </head>
+    <body>
+        <div ng-app="app">
+            <h1>app</h1>
+            <p ng-model="model">Model</p>
+        </div>
+    </body>
+</html>
