4b6d8826f7d894cd9001ca6d4527d7a448486aea
jeanSaposhnikov <jean.saposhnikov@hotmail.fr>
2023-08-25 15:16:53 -0400

    price system

--- index-advanced-2.php
+++ index-advanced-2.php
@@ -1,9 +1,17 @@
 <!DOCTYPE HTML>
 <html>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
-<body ng-app="app" ng-controller="controller">
+<body ng-app="app" ng-controller="controller" ng-init="price=0;quantity=0">
     <p>Coolest Name : {{clients[2]}}</p>
     <input type="text" ng-model="color" backgroundColor>
+
+    <p>What is your name ? :</p> <input type="text" ng-model="name">
+    <p>Nice to meet you {{name}} ! </p>
+
+    <p>How many items you wish to buy ?</p>
+    <input type="number" ng-model="quantity">
+    <p>That would cost you {{price}} $ !</p>
+
 
     <script src="js/app.js"></script>
     <script src="js/controller.js"></script>
--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,6 @@
 app.controller("controller", function($scope){
     $scope.clients = ['Robert','Celine','Azazel','Mick','Nicole']
+    $scope.price=quantity * 3
 });
 
 app.directive("backgroundColor", function(){
