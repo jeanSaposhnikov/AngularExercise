e8fbbc4710e9f9a044941ceaab1fad01b4bc9746
jeanSaposhnikov <jean.saposhnikov@hotmail.fr>
2023-09-01 10:39:25 -0400

    js and css files created

--- result/index.php
+++ result/index.php
@@ -1,9 +1,13 @@
 <!doctype HTML>
 <head>
+    <link rel="stylesheet" href="css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
 </head>
 <html>
 <body ng-app="app" ng-controller="controlleur">
-
+    <p ng-model="test">{{test}}</p>
 </body>
 </html>
+
+<script src="js/app.js"></script>
+<script src="js/controlleur.js"></script>
---
+++ result/js/controlleur.js
@@ -0,0 +1,3 @@
+app.controller("controlleur", function($scope){
+    $scope.test="test reussi";
+});
