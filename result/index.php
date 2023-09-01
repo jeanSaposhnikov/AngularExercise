a0803837c4392e81d49fd7cc15d567fdef52ed8f
jeanSaposhnikov <jean.saposhnikov@hotmail.fr>
2023-09-01 10:21:04 -0400

    what I set up myself

---
+++ result/index.php
@@ -0,0 +1,9 @@
+<!doctype HTML>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+</head>
+<html>
+<body ng-app="app" ng-controller="controlleur">
+
+</body>
+</html>
