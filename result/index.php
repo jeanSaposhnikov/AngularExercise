c4315f4269630bbe499f3ae77c874cb8bb7a4fde
jeanSaposhnikov <jean.saposhnikov@hotmail.fr>
2023-09-01 11:16:56 -0400

    Exam done before final correction

--- result/index.php
+++ result/index.php
@@ -5,7 +5,33 @@
 </head>
 <html>
 <body ng-app="app" ng-controller="controlleur">
-    <p ng-model="test">{{test}}</p>
+    <p ng-model="ranking">Coolest Name : {{ranking[2]}}</p>
+    <input type="text">
+    <p>What is your name ? :</p>
+    <input type="text" name="name" ng-model="name">
+    <p ng-show="name">Nice to meet you {{name}} !</p>
+
+    <p>How many items do you wish to buy ?</p>
+    <input type="number" ng-model="quantity">
+    <p ng-show="quantity">That would cost you {{quantity * 3.5}}</p>
+
+    <p>Here is the list of clients :</p>
+    <ul>
+        <li ng-repeat="x in clients">{{x}}</li>
+    </ul>
+
+    <p>And here is a list of targets :</p>
+    <ul>
+        <li ng-repeat="x in targets">{{x.name + " from " + x.location}}</li>
+    </ul>
+
+    <p>Fill the fields below</p>
+    <form>
+        <label>Name<input type="text" name="name"></label>
+        <label>Email<input type="text" name="email"></label><span ng-show="form.email.email.$invalid">The email is invalid</span>
+        <label>Phone<input type="text" name="tel"></label>
+        <label>Test Field :<input type="text" name="test"></label><span>The field has been touched !</span>
+    </form>
 </body>
 </html>
 
--- result/js/controlleur.js
+++ result/js/controlleur.js
@@ -1,3 +1,18 @@
 app.controller("controlleur", function($scope){
-    $scope.test="test reussi";
+    $scope.ranking=["Abdul", "Cooler","Azazel"];
+    $scope.clients= [
+        "Robert",
+        "Celine",
+        "Azazel",
+        "Mick",
+        "Nicole",
+        "Ken"
+    ];
+
+    $scope.targets= [
+            {name:"Bowser", location: "Denmark"},
+            {name:"Luigi", location: "Italy"},
+            {name:"Yoshi", location: "Marshlands"},
+            {name:"Lucio", location: "Amsterdam"}
+    ]
 });
